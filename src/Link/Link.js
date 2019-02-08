19c6bf915708ba7db56f483278cc7ae2aae04e59
Gahnterr <aoliva@bts.net>
2019-02-08 12:52:29 -0800

    Agregar font awesome (incompleto)

--- src/Link/Link.js
+++ src/Link/Link.js
@@ -3,8 +3,15 @@ import PropTypes from 'prop-types';
 import colors from '../styles/colors.json';
 import typography from '../styles/typography.json';
 
+const styles = `
+    font-size: ${typography.size.l}px;
+    font-weight: ${typography.weight.regular};
+    color: ${colors.default.secundario.base};
+    text-decoration: underline;
+`;
+
 const Link = props => {
-  return <a>{props.children}</a>;
+  return <a css={styles}>{props.children}</a>;
 };
 
 Link.propTypes = {
