66640052d4b05ecd516cec669f584941b12c3ccd
Gahnterr <aoliva@bts.net>
2019-02-19 14:38:19 -0800

    Por fin lo hice funcionar otra vez :D
    No quitar los loaders de las tipografías de font-awesome!!!!

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,14 @@ module.exports = {
         test: /\.js$/,
         loader: 'source-map-loader',
       },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      },
     ],
   },
 };
