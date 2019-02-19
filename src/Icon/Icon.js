bd6a0bc2cf21990e77e0a182979f196c967670bb
Gahnterr <aoliva@bts.net>
2019-02-19 11:55:19 -0800

    Agregado FontAwesome Icon

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -1,12 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import 'font-awesome/css/font-awesome.min.css';
+import '../styles/styles.css';
 
-const Icon = () => {
-  return <i className="fa fa-spinner" />;
+const Icon = props => {
+  let iconColor;
+  switch (props.color) {
+    case 'texto-regular':
+      iconColor = 'var(--texto-regular)';
+      break;
+    case 'texto-auxiliar':
+      iconColor = 'var(--texto-auxiliar';
+      break;
+    case 'primario':
+      iconColor = 'var(--primario-base)';
+      break;
+    case 'secundario':
+      iconColor = 'var(--secundario-base)';
+      break;
+    case 'error':
+      iconColor = 'var(--error)';
+      break;
+    case 'advertencia':
+      iconColor = 'var(--advertencia)';
+      break;
+    case 'exito':
+      iconColor = 'var(--exito)';
+      break;
+    default:
+      iconColor = 'var(--texto-regular)';
+  }
+
+  const stylesIcon = `color: ${iconColor}`;
+
+  return (
+    <i
+      className={`fa fa-${props.icon} fa-${props.size} ${props.spin ? 'fa-spin' : null} ${props.spin && props.pulse ? 'fa-pulse' : null}`}
+      css={stylesIcon}
+    />
+  );
 };
 
 Icon.propTypes = {
+  icon: PropTypes.string,
   size: PropTypes.oneOf (['lg', '2x', '3x', '4x', '5x']),
+  color: PropTypes.oneOf ([
+    'texto-regular',
+    'texto-auxiliar',
+    'primario',
+    'secundario',
+    'error',
+    'advertencia',
+    'exito',
+  ]),
+  spin: PropTypes.bool,
+  pulse: PropTypes.bool,
 };
 
 export {Icon as default};
--- src/Icon/presets/0-default.jsx
+++ src/Icon/presets/0-default.jsx
@@ -1,3 +1,3 @@
 import Icon from '../Icon.js';
 
-export default <Icon uxpId="1" />;
+export default <Icon uxpId="1" icon="fa" size="2x" color="texto-regular" />;
