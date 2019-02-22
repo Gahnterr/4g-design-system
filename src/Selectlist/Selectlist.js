6e1a33f2891b2d5a2ba3a02e977c264d8ee4f0dd
Gahnterr <aoliva@bts.net>
2019-02-21 17:09:41 -0800

    Configurando Selectlist para que tenga un dropdown list

--- src/Selectlist/Selectlist.js
+++ src/Selectlist/Selectlist.js
@@ -4,30 +4,89 @@ import {PropTypes} from 'prop-types';
 
 export default class Selectlist extends React.Component {
   state = {
-    value: '',
+    selected: '',
     focus: false,
+    options: this.props.options,
   };
 
-  handleFocus = () => {};
+  handleFocus = () => {
+    this.setState ({focus: true});
+  };
+
+  handleBlur = () => {
+    this.setState ({focus: false});
+  };
+
+  handleClick = () => {};
 
   render () {
+    const state = this.state;
+
     return (
-      <div className="selectlist">
-        <input
-          className="selectlist__input"
-          type="text"
-          placeholder="Seleccione una opción"
-        />
-        <Icon
-          className="selectlist__icono"
-          icon="caret-down"
-          color="texto-regular"
-        />
-      </div>
+      <React.Fragment>
+        <div
+          className={`selectlist${state.focus ? ' selectlist--focus' : ' selectlist--blur'}`}
+        >
+          <input
+            className="selectlist__input"
+            type="text"
+            placeholder="Seleccione una opción"
+            onFocus={this.handleFocus}
+            onBlur={this.handleBlur}
+          />
+          <Icon
+            className="selectlist__icono"
+            icon="caret-down"
+            color="texto-regular"
+            onClick={state.focus ? this.handleBlur : this.handleFocus}
+          />
+        </div>
+        {state.focus
+          ? <div className="dropdown">
+              <ul className="dropdown__list">
+                <li
+                  className="dropdown__list__item"
+                  id="0"
+                  onClick={this.handleClick}
+                >
+                  Test
+                </li>
+                <li
+                  className="dropdown__list__item"
+                  id="1"
+                  onClick={this.handleClick}
+                >
+                  Segunda opción
+                </li>
+                <li
+                  className="dropdown__list__item"
+                  id="2"
+                  onClick={this.handleClick}
+                >
+                  Otra opción
+                </li>
+                <li
+                  className="dropdown__list__item"
+                  id="3"
+                  onClick={this.handleClick}
+                >
+                  México
+                </li>
+                <li
+                  className="dropdown__list__item"
+                  id="4"
+                  onClick={this.handleClick}
+                >
+                  No México
+                </li>
+              </ul>
+            </div>
+          : null}
+      </React.Fragment>
     );
   }
 }
 
 Selectlist.propTypes = {
-  options: PropTypes.string,
+  options: PropTypes.array,
 };
