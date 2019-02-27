d33c33bf226b3797f0c97577220c1a273003c192
Gahnterr <aoliva@bts.net>
2019-02-26 17:23:43 -0800

    Update Selectlist.js

--- src/Selectlist/Selectlist.js
+++ src/Selectlist/Selectlist.js
@@ -9,39 +9,57 @@ export default class Selectlist extends React.Component {
     value: '',
     dropdownIsOpen: false,
     options: this.props.options,
+    isSearching: false,
+    searchResults: [],
   };
 
   selectlistInput = React.createRef ();
 
-  toggleDropdown = e => {
-    if (e.target.classList.value.includes ('dropdown__item')) {
-      this.selectItem (e.target.innerText);
-    } else {
-      this.state.dropdownIsOpen
-        ? this.closeDropdown (this.state.selected)
-        : this.openDropdown ();
-    }
-  };
-
   openDropdown = () => {
     this.setState ({dropdownIsOpen: true});
-    this.selectlistInput.current.focus ();
+    this.selectlistInput.current.addEventListener (
+      'focusout',
+      this.closeDropdown,
+      false
+    );
   };
 
-  closeDropdown = selected => {
-    this.setState ({value: selected, dropdownIsOpen: false});
-    this.selectlistInput.current.blur ();
+  closeDropdown = () => {
+    this.setState ({
+      value: this.state.selected,
+      dropdownIsOpen: false,
+      isSearching: false,
+    });
+    this.selectlistInput.current.removeEventListener ();
+  };
+
+  buildDropdown = options => {
+    return options.map (option => (
+      <li className="dropdown__item" onClick={this.selectItem}>{option}</li>
+    ));
+  };
+
+  selectItem = e => {
+    e.preventPropagation ();
+    this.setState ({
+      selected: e.target.innerText,
+      value: e.target.innerText,
+      dropdownIsOpen: false,
+    });
   };
 
   searchItem = e => {
-    this.setState ({value: e.target.value});
-  };
-
-  selectItem = targetInnerText => {
-    this.setState ({
-      selected: targetInnerText,
+    const searchTerm = RegExp (e.target.value);
+    const searchResults = this.state.options.map (option => {
+      if (searchTerm.test (option)) {
+        return option;
+      }
     });
-    this.closeDropdown (targetInnerText);
+    this.setState ({
+      value: e.target.value,
+      // isSearching: true,
+      searchResults: searchResults,
+    });
   };
 
   render () {
@@ -53,7 +71,6 @@ export default class Selectlist extends React.Component {
         {props.label ? <Label>{props.label}</Label> : null}
         <div
           className={`selectlist${state.dropdownIsOpen ? ' selectlist--focus' : ' selectlist--blur'}`}
-          onClick={this.toggleDropdown}
         >
           <input
             className="selectlist__input"
@@ -61,25 +78,23 @@ export default class Selectlist extends React.Component {
             placeholder="Seleccione una opción"
             ref={this.selectlistInput}
             value={state.value}
+            onFocus={this.openDropdown}
+            /* onBlur={this.closeDropdown} */
             onChange={this.searchItem}
           />
           <Icon
             className="selectlist__icono"
             icon="caret-down"
             color="texto-regular"
+            onClick={() =>
+              state.dropdownIsOpen
+                ? this.selectlistInput.current.blur ()
+                : this.selectlistInput.current.focus ()}
             size={null}
           />
           {state.dropdownIsOpen
-            ? <ul className="dropdown default-scrollbar">
-                {state.options
-                  ? state.options.map (option => (
-                      <li className="dropdown__item" key={option.toString ()}>
-                        {option}
-                      </li>
-                    ))
-                  : <li className="dropdown__item" disabled>
-                      No existen registros...
-                    </li>}
+            ? <ul className="dropdown">
+                {this.buildDropdown (state.options)}
               </ul>
             : null}
         </div>
