820ff2648124151c1218a01c478f02cdcaab3509
Gahnterr <aoliva@bts.net>
2019-02-27 17:26:58 -0800

    Update Back.js

--- src/Back/Back.js
+++ src/Back/Back.js
@@ -5,7 +5,12 @@ import Icon from '../Icon/Icon';
 const Back = props => {
   return (
     <button className="btn btn--regresar" type="button" onClick={props.onClick}>
-      <Icon className="btn__icono--regresar" icon="arrow-left" color="blanco" />
+      <Icon
+        className="btn__icono--regresar"
+        icon="arrow-left"
+        color="blanco"
+        size={null}
+      />
     </button>
   );
 };
