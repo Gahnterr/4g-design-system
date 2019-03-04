46df98e23b17b3b49a9c6e23772157ad87f780ba
Gahnterr <aoliva@bts.net>
2019-03-04 15:47:15 -0800

    Actualizar Label para poder recibir prop "disabled"

--- src/Label/Label.js
+++ src/Label/Label.js
@@ -2,15 +2,24 @@ import React from 'react';
 import {PropTypes} from 'prop-types';
 
 const Label = props => {
-  return <label className="temis-label">{props.children}</label>;
+  return (
+    <label
+      className={`temis-label${(props.disabled ? ' temis-label--disabled' : '') + props.className}`}
+    >
+      {props.children}
+    </label>
+  );
 };
 
 Label.propTypes = {
   children: PropTypes.string,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
 };
 
 Label.defaultProps = {
   children: 'Etiqueta:',
+  disabled: false,
 };
 
 export default Label;
