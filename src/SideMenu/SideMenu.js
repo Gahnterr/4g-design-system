20125b74825108ecbb57a304ff3cf63c89caf63f
Gahnterr <aoliva@bts.net>
2019-03-28 17:15:33 -0700

    Update SideMenu.js

--- src/SideMenu/SideMenu.js
+++ src/SideMenu/SideMenu.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {cloneElement, Children} from 'react';
 import {PropTypes} from 'prop-types';
 import SideMenuItem from '../SideMenuItem/SideMenuItem';
 
 export default class SideMenu extends React.Component {
   state = {activeMenuItem: 0};
 
+  toggleActiveMenuItem = e => {
+    this.setState ({activeMenuItem: e.target.value});
+  };
+
   render () {
-    const props = this.props;
+    const children = Children.map (this.props.children, child => {
+      return cloneElement (child, {
+        isActive: true,
+      });
+    });
 
     return (
       <aside className="host-side-menu">
         <ul className="host-side-menu__list">
-          {props.children}
+          {children}
         </ul>
         <footer className="host-side-menu__footer">
           <SideMenuItem icon="arrow-left" text="Lista de Modelos" />
