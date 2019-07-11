b6e68b635c7c21776c1d1bd4b790b7bf9cfa5092
Gahnterr <45857109+Gahnterr@users.noreply.github.com>
2019-07-11 10:27:56 -0700

    Ajustando presets para el nuevo componente

--- src/Selectlist/presets/0-default.jsx
+++ src/Selectlist/presets/0-default.jsx
@@ -1,21 +1,6 @@
 import React from 'react';
 import Selectlist from '../Selectlist';
-import DropdownItem from './../../DropdownItem/DropdownItem';
 
 export default (
-  <Selectlist uxpId="selectlist.1" label="Elige un país:">
-    <DropdownItem uxpId="selectlist.item.1">México</DropdownItem>
-    <DropdownItem uxpId="selectlist.item.2">Estados Unidos</DropdownItem>
-    <DropdownItem uxpId="selectlist.item.3">Canadá</DropdownItem>
-    <DropdownItem uxpId="selectlist.item.4">Guatemala</DropdownItem>
-    <DropdownItem uxpId="selectlist.item.5">Panamá</DropdownItem>
-    <DropdownItem uxpId="selectlist.item.6">Chile</DropdownItem>
-    <DropdownItem uxpId="selectlist.item.7">Brasil</DropdownItem>
-    <DropdownItem uxpId="selectlist.item.8">Ecuador</DropdownItem>
-    <DropdownItem uxpId="selectlist.item.10">Colombia</DropdownItem>
-    <DropdownItem uxpId="selectlist.item.11">Venezuela</DropdownItem>
-    <DropdownItem uxpId="selectlist.item.12">Nicaragua</DropdownItem>
-    <DropdownItem uxpId="selectlist.item.13">Paraguay</DropdownItem>
-    <DropdownItem uxpId="selectlist.item.14">Honduras</DropdownItem>
-  </Selectlist>
+  <Selectlist uxpId="selectlist.1" label="Elige un país:" options={["México", "Estados Unidos", "Panamá"]} />
 );
