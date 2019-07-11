efdb649c63b6004ef091c46ffbfe2a68025a85eb
Gahnterr <45857109+Gahnterr@users.noreply.github.com>
2019-07-11 10:18:29 -0700

    Cambiar Seleclist a un selectlist HTML
    Se convierte a un componente más sencillo, hasta que uno más avanzado se pueda crear por separado.

--- src/Selectlist/Selectlist.js
+++ src/Selectlist/Selectlist.js
@@ -1,97 +1,28 @@
 import React from 'react';
-import Icon from '../Icon/Icon';
 import Label from '../Label/Label';
 import {PropTypes} from 'prop-types';
-import Dropdown from '../Dropdown/Dropdown';
-import DropdownItem from '../DropdownItem/DropdownItem';
-import Input from '../Input/Input';
 
-export default class Selectlist extends React.Component {
-  state = {
-    defaultValue: this.props.defaultValue,
-    value: '',
-    dropdownIsOpen: this.props.dropdownIsOpen,
-  };
-
-  selectlistInput = React.createRef ();
-
-  componentDidMount = () => {
-    document.addEventListener ('click', this.handleClickOutside);
-  };
-
-  componentWillUnmount = () => {
-    document.removeEventListener ('click', this.handleClickOutside);
-  };
-
-  handleClickOutside = e => {
-    if (e.target.nodeName === 'LI') {
-      this.selectItem (e);
-    } else if (e.target.nodeName === 'I' || e.target.nodeName === 'INPUT') {
-      null;
-    } else {
-      this.closeDropdown ();
-    }
-  };
-
-  openDropdown = () => {
-    this.setState ({dropdownIsOpen: true});
-    this.selectlistInput.current.focus ();
-  };
-
-  closeDropdown = () => {
-    this.setState ({
-      value: this.state.selected,
-      dropdownIsOpen: false,
-    });
-  };
-
-  selectItem = e => {
-    this.setState ({
-      selected: e.target.innerText,
-      value: e.target.innerText,
-      dropdownIsOpen: false,
-    });
-  };
-
-  render () {
-    const state = this.state;
-    const props = this.props;
-
-    return (
+const Selectlist = props => {
+  const {disabled, label, options} = props; 
+  
+  const renderOptions = return options.map((option, index) => (
+    <option className="temis-selectlist__option">{option}</option>
+  ));
+  
+  return (
       <React.Fragment>
-        {props.label
+        {label
           ? <Label>
-              {props.label}
+              {label}
             </Label>
           : null}
-        <div
-          className={`temis-selectlist${(state.dropdownIsOpen ? ' temis-selectlist--focus' : ' temis-selectlist--blur') + (props.disabled ? ' temis-selectlist--disabled' : '')}`}
-          onClick={
-            state.dropdownIsOpen ? this.closeDropdown : this.openDropdown
-          }
+        <select
+          className="temis-selectlist"
         >
-          <Input
-            label={null}
-            className="temis-selectlist__input"
-            type="text"
-            placeholder="Seleccione una opción"
-            ref={this.selectlistInput}
-            value={state.value}
-          />
-          <Icon
-            className="temis-selectlist__icono"
-            icon="caret-down"
-            color="texto-regular"
-          />
-        </div>
-        <Dropdown isOpen={state.dropdownIsOpen}>
-          {props.children
-            ? props.children
-            : <DropdownItem disabled>No existen registros...</DropdownItem>}
-        </Dropdown>
+    {renderOptions}
+        </select>
       </React.Fragment>
     );
-  }
 }
 
 Selectlist.propTypes = {
@@ -99,16 +30,14 @@ Selectlist.propTypes = {
    * Si lo dejas vacío, no se renderizará la etiqueta.  */
   label: PropTypes.string,
   /** Introduce componentes "SelectItem" para popular el dropdown de este componente. */
-  children: PropTypes.node,
+  options: PropTypes.array,
   /**
    * Indica el valor precargado por defecto.
    */
   defaultValue: PropTypes.string,
   disabled: PropTypes.bool,
-  dropdownIsOpen: PropTypes.bool,
 };
 
 Selectlist.defaultProps = {
   label: 'Países:',
-  dropdownIsOpen: false,
 };
